0c732faaa7572fe6e5220e679d7666c4326633b2
Edward Kim <itgma1231@gmail.com>
2023-08-18 13:18:47 -0400

    set up ./sql folder for mysql schemas & queries

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+// CommonJS import statements; inquirer@8.2.4, ascii-art, mysql2
 const inquirer = require("inquirer");
 const art = require("ascii-art");
 const mysql = require("mysql2");
 
+// Create a connection pool with mysql2
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    database: ''
+    password: '',
+    database: '',
 }).promise()
 
+// Render ASCII Art at the start of application
 const asciiArt = () => {
   art.font("Employee Tracker", "doom").then((rendered) => {
     console.log(rendered);
