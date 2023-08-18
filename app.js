11162e397ee29ccdef0d3f88c21b6263f89a2472
Edward Kim <itgma1231@gmail.com>
2023-08-18 12:53:15 -0400

    createPool().promise() with mysql2

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const inquirer = require("inquirer");
 const art = require("ascii-art");
 const mysql = require("mysql2");
 
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    database: ''
+}).promise()
+
 const asciiArt = () => {
   art.font("Employee Tracker", "doom").then((rendered) => {
     console.log(rendered);
@@ -10,5 +16,3 @@ const asciiArt = () => {
     }, 3000);
   });
 };
-
-asciiArt();
