a0ee3b29f4176f49a82d4c4b800b61bf7596b13c
Edward Kim <itgma1231@gmail.com>
2023-08-18 12:46:26 -0400

    created function for ascii art

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const inquirer = require('inquirer');
-const art = require('ascii-art');
-const mysql = require('mysql2')
+const inquirer = require("inquirer");
+const art = require("ascii-art");
+const mysql = require("mysql2");
 
+const asciiArt = () => {
+  art.font("Employee Tracker", "doom").then((rendered) => {
+    console.log(rendered);
+    setTimeout(() => {
+      console.clear();
+    }, 3000);
+  });
+};
+
+asciiArt();
